d15962af026b23d9355bae0aa38971a08c6aaffc
tompol1123581321 <tom.polivka96@gmail.com>
2022-11-25 15:48:30 +0100

    added dot env and connectiont to db

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('test')
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+
+
+
+const uri = process.env.MONGO_DB_URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('connected',collection,err)
+
+  // perform actions on the collection object
+  client.close();
+});
