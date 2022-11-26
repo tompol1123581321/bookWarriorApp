b197655700609095457df1545654104766d65379
tompol1123581321 <tom.polivka96@gmail.com>
2022-11-26 21:47:39 +0100

    Added react

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ const __dirname = path.dirname(__filename);
 const mongo = await connectToDb();
 
 const app = express();
-const port = 3000;
+const port = 3001;
+const reactBuild = path.join(__dirname, "client", "build");
+app.use(express.static(reactBuild));
 
 const DBinstance = mongo.db("BookWarriorDB").collection("Users");
 const x = mongo.db("BookWarriorDB").collection("Users");
 
 //mongo.db("BookWarriorDB").collection("Users").insertMany(users);
 //console.log(users)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/pages/index.html"));
-  // tady se bude servovat html
+app.get("/api", async (req, res) => {
+  res.send({ message: "Hello" });
+});
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(reactBuild, "index.html"));
 });
 
 app.listen(port, () => {
