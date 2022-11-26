669f89b683b06cb97e5208f240e88a51bcad64af
tompol1123581321 <tom.polivka96@gmail.com>
2022-11-26 11:41:08 +0100

    serving html from express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { connectToDb } from "./db/index.js";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 await connectToDb();
 
@@ -7,7 +11,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "/index.html"));
   // tady se bude servovat html
 });
 
