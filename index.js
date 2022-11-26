bcb095db483e06e444bce36bb564a37ba57dfaec
tompol1123581321 <tom.polivka96@gmail.com>
2022-11-26 10:48:07 +0100

    Added express framework

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import { connectToDb } from "./db/index.js";
+import express from "express";
 
 await connectToDb();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
