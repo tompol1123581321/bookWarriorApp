073a74f46bbca02e7c42b0b13abeaf025cb10c79
LGazdik <58519165+LGazdik@users.noreply.github.com>
2022-11-27 19:35:20 +0100

    admin index list

--- client/src/components/Admin/index.tsx
+++ client/src/components/Admin/index.tsx
@@ -1,5 +1,28 @@
+import { ListItem, List, ListItemText, Divider } from "@mui/material";
+
 import React from "react";
 
 export const AdminPages = () => {
-  return <div>AdminPages</div>;
+  return (
+    <div>
+      <h1>AdminPages</h1>
+      <div>
+        <h3>Zoznam uživateľov</h3>
+        <List>{AllLine()}</List>
+      </div>
+    </div>
+  );
+};
+
+const AllLine = () => {
+  let content = [];
+  for (let i = 0; i < 10; i++) {
+    content.push(
+      <ListItem sx={{justifyContent: "flex-start"}}>
+        <ListItemText primary="Meno: Person #{i}" secondary="rodné číslo" />
+        <ListItemText primary="Meno: Person #{i}" secondary="rodné číslo" />
+      </ListItem>
+    );
+  }
+  return content;
 };
