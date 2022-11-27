fd49f2fde299430134b937214ca2c5f0037b78d3
Tomas Polivka <t_pol@pop-os.localdomain>
2022-11-27 17:38:14 +0100

    login form data

--- client/src/components/Login/LoginForm.tsx
+++ client/src/components/Login/LoginForm.tsx
@@ -1,30 +1,53 @@
-import { Button, Paper, TextField } from "@mui/material";
+import { Button, Paper, TextField, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = React.useState({
+    userName: "",
+    password: "",
+  });
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const key = e.target.id;
+    setFormData({ ...formData, [key]: e.target.value });
+  };
+  const onSubmit = React.useCallback(() => {
+    // post data and validate it
+    // if success
+    //  route to admin or visitor
+    navigate("/admin");
+  }, [formData]);
   return (
     <Paper
       elevation={3}
       component={"div"}
       sx={{
         padding: "3rem",
-        maxWidth: "25rem",
-        height: "15rem",
+        width: "20rem",
+        height: "20rem",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-evenly",
       }}
     >
-      <TextField id="userName" label="UserName" defaultValue="Hello World" />
-      <TextField id="password" label="Password" defaultValue="Hello World" />
-      <Button
-        variant="contained"
-        onClick={() => {
-          navigate("/admin");
-        }}
-      >
+      <Typography sx={{ mb: "1rem" }} variant={"h4"} align="center">
+        Please log in
+      </Typography>
+      <TextField
+        onChange={onChange}
+        value={formData.userName}
+        id="userName"
+        label="UserName"
+      />
+      <TextField
+        onChange={onChange}
+        value={formData.password}
+        type={"password"}
+        id="password"
+        label="Password"
+      />
+      <Button variant="contained" onClick={onSubmit}>
         Login
       </Button>
     </Paper>
--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
@@ -5,7 +6,19 @@ import { LoginForm } from "./LoginForm";
 
 export const LoginPages = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography marginBottom={"5rem"} variant="h3">
+        BookWarriorApp
+      </Typography>
       <LoginForm />
     </Box>
   );
