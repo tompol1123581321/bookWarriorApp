c9fe0e89841a53a47e9f9d4a88213cead30937d7
LGazdik <58519165+LGazdik@users.noreply.github.com>
2022-11-30 22:00:56 +0100

    added lists to admin index

--- db/operations.js/getUsers.js
+++ db/operations.js/getUsers.js
@@ -1,7 +1,6 @@
 import { getDbClient } from "../connect.js";
 
 export const getUsers = async (params = {}) => {
-  const client = getDbClient();
   if (client) {
     const data = await client.collection("Users").find(params).toArray();
     return data;
