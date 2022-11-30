0362a44354881ccaadfb9b74b5e22efa67210925
LGazdik <58519165+LGazdik@users.noreply.github.com>
2022-11-30 22:02:32 +0100

    added lists admin ind

--- client/src/components/Admin/index.tsx
+++ client/src/components/Admin/index.tsx
@@ -1,37 +1,110 @@
-import { ListItem, List, ListItemText, Divider, Typography } from "@mui/material";
-
+import { ListItem, List, ListItemText, Typography, Box, Grid, Button } from "@mui/material";
 import React from "react";
+
+function AddNewBookToDB(){};
+function AcceptUser(){};
+function BanUser(){};
+function DeleteBookFromDB(){};
+function EditBook(){};
 
 export const AdminPages = () => {
   return (
-    <div>
+    <>
       <h1>AdminPages</h1>
-      <div>
-        <h3>Zoznam uživateľov</h3>
-        <List sx={{ width: "70%" }}>{AllLine()}</List>
-      </div>
-    </div>
+      <Grid container direction="row" justifyContent="space-around">
+        <Grid item xs={5}>
+          <Typography variant="h5" sx={{ p: 3 }}>
+            Zoznam uživateľov
+          </Typography>
+          <List>{RenderUsers()}</List>
+        </Grid>
+        <Grid item xs={5}>
+          <Grid container>
+            <Typography variant="h5" sx={{ p: 3 }}>
+              Zoznam kníh
+            </Typography>
+            <Button onClick={AddNewBookToDB}>Add new book</Button>
+          </Grid>
+          <List>{RenderBooks()}</List>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
-const AllLine = () => {
+const RenderUsers = () => {
   let content = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     content.push(
       <>
-        <ListItem alignItems="flex-start">
-          <ListItemText
-            primary="Meno: Person"
-            secondary={
-              <React.Fragment>
-                <Typography>Rodné číslo: </Typography>
-                <Typography>Adresa: </Typography>
-                <Typography>Požičané knihy: </Typography>
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider />
+        <Box>
+          <ListItem sx={{ boxShadow: 1 }}>
+            <Grid container direction="row" alignItems="start">
+              <Grid item>
+                <ListItemText
+                  sx={{ p: 0.5 }}
+                  primary="Vladolf Putler"
+                  secondary={
+                    <React.Fragment>
+                      <Typography>Rodné číslo: </Typography>
+                      <Typography>Adresa: </Typography>
+                      <Typography>Počet požičaných kníh: </Typography>
+                    </React.Fragment>
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item xs={6}>
+                <Grid>
+                  <Button onClick={AcceptUser}>Accept user</Button>
+                </Grid>
+                <Grid>
+                  <Button onClick={BanUser}>Ban user</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </ListItem>
+        </Box>
+      </>
+    );
+  }
+  return content;
+};
+
+const RenderBooks = () => {
+  let content = [];
+  for (let i = 0; i < 5; i++) {
+    content.push(
+      <>
+        <Box>
+          <ListItem sx={{ boxShadow: 1 }}>
+            <Grid container direction="row" alignItems="start">
+              <Grid item>
+                <ListItemText
+                  sx={{ p: 0.5 }}
+                  primary="Title: "
+                  secondary={
+                    <React.Fragment>
+                      <Typography>Author: </Typography>
+                      <Typography>Available licences: </Typography>
+                    </React.Fragment>
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item xs={6}>
+                <Grid>
+                  <Button onClick={DeleteBookFromDB}>Delete book</Button>
+                </Grid>
+                <Grid>
+                  <Button onClick={EditBook}>Edit book</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </ListItem>
+        </Box>
       </>
     );
   }
